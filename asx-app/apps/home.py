691446b26c6accc512fcb1d5e89ed6ba6091a773
noahrubin989 <77037165+noahrubin989@users.noreply.github.com>
2023-01-12 13:52:13 +1100

    Delete asx-app directory

--- asx-app/apps/home.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-def app():
-    st.title('Home')
-    st.write('This is the `home` page of this multi-page app.')
-    st.write('In this app, we explore financial data from many different angles. Enjoy!')
